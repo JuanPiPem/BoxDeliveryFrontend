f813b4beb28f4c9ffd80f0c77671ad15b3347fe5
bautistagorchs <bautista.gorchs@gmail.com>
2024-01-17 10:47:13 -0300

    testing husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { useState } from "react";
+
 export default function Home() {
   return (
     <main className={styles.main}>
