57e38890ee03ab99c101e5d5abe59703a1675c1f
FabiolaLutrario <fabiolalutrario@gmail.com>
2024-01-20 22:34:56 -0300

    Updating SASS configuration Next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import styles from "./page.module.css";
 import React from "react";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <h1>Projecto Levantado!!</h1>
     </main>
   );
