795825b90d4d6770c3591c1ab148dc42fa91b5ed
FabiolaLutrario <fabiolalutrario@gmail.com>
2024-01-20 22:56:53 -0300

    Updating SASS configuration Next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
+
+module.exports = nextConfig;
