db3b68425728449769f4caa7e909c9e458edb1d0
rosmari ledesma <rochilesde@gmail.com>
2024-01-23 17:04:34 -0300

    added admin dispatchers view

---
+++ src/app/DeliverymenAdmin/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import DeliverymenAdmin from "components/deliverymenAdmin/DeliverymenAdmin";
+const deliverymenAdmin= () => {
+  return (
+    <DeliverymenAdmin/>
+  );
+};
+
+export default deliverymenAdmin;
