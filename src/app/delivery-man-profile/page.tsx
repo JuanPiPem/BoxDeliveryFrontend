202455776afea1f390786a741bab762a11ac4f95
JuanPiPem <jplezcano75@gmail.com>
2024-01-24 07:13:59 -0800

    adding delivery-man-profile view

---
+++ src/app/delivery-man-profile/page.tsx
@@ -0,0 +1,13 @@
+import DeliveryManProfile from "components/deliveryManProfile/DeliveryManProfile";
+
+import React from "react";
+
+const delivery = () => {
+  return (
+    <div>
+      <DeliveryManProfile />
+    </div>
+  );
+};
+
+export default delivery;
