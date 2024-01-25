d3f1f2849a9fa124b0e9d093ebf69781d77c657f
bautistagorchs <bautista.gorchs@gmail.com>
2024-01-25 12:15:38 -0300

    add eye for password to login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import s from "./login.module.scss";
 import LoginBoxLogo from "assets/img/LoginBoxLogo";
 import ButtonDarkBlue from "commons/buttonDarkBlue/ButtonDarkBlue";
+import Eye from "assets/img/Eye";
 
 /* Si el login es de tipo repartidor(state Redux): hacer un classList.remove de la clase "s.heigthContentContainer1" y un classList.toggle de "s.heigthContentContainer2"; y también hacer un classList.remove del button que tiene la clase "s.displayNone" */
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div className={s.loginContainer}>
       <div
@@ -17,11 +21,19 @@ const Login = () => {
             className={`${s.input}`}
             placeholder="Email@example.com"
           />
-          <input
-            type="password"
-            className={`${s.input}`}
-            placeholder="***********"
-          />
+          <div className={s.inputPasswordContainer}>
+            <input
+              type={showPassword ? `text` : `password`}
+              className={`${s.input}`}
+              placeholder="***********"
+            />
+            <div
+              className={s.eyeContainer}
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              <Eye />
+            </div>
+          </div>
           <div className={s.buttonLogin}>
             <ButtonDarkBlue text="Ingresar" />
           </div>
