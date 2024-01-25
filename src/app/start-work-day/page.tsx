d593ffc191f1de60dd6cbacc75260ce1a11d604e
FabiolaLutrario <fabiolalutrario@gmail.com>
2024-01-25 16:47:07 -0300

    Adding route start-work-day

---
+++ src/app/start-work-day/page.tsx
@@ -0,0 +1,12 @@
+import StartWorkDay from "components/startWorkDay/StartWorkDay";
+import React from "react";
+
+const startWorkDay = () => {
+  return (
+    <div>
+      <StartWorkDay />
+    </div>
+  );
+};
+
+export default startWorkDay;
