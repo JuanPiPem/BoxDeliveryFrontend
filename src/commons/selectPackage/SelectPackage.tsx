4a10f362071b3dc060a0be1f55ca9d26b3d8bd34
FabiolaLutrario <fabiolalutrario@gmail.com>
2024-01-30 09:04:00 -0300

    Adding common selectPackage

---
+++ src/commons/selectPackage/SelectPackage.tsx
@@ -0,0 +1,24 @@
+import Unchecked from "assets/img/Unchecked";
+import s from "./selectPackage.module.scss";
+import React from "react";
+import Checked from "assets/img/Checked";
+
+type Prop = {
+  address: string;
+  checked: boolean;
+};
+
+function SelectPackage(prop: Prop) {
+  return (
+    <>
+      <div className={s.addressList}>
+        {prop.checked ? <Checked /> : <Unchecked />}
+
+        <div className={s.address}>{prop.address}</div>
+      </div>
+      <hr className={s.lastHr} />
+    </>
+  );
+}
+
+export default SelectPackage;
