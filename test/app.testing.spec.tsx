7ca63cf8626984ec0f7da368f18799c9271abf6a
bautistagorchs <bautista.gorchs@gmail.com>
2024-02-07 23:04:19 -0300

    add jest config

---
+++ test/app.testing.spec.tsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+describe("probando los test", () => {
+  test("tiene que ir bien", () => {
+    expect(true).toStrictEqual(true);
+  });
+});
