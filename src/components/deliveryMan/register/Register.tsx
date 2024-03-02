37bcc90a330dd3f00513a0e84ac10b7de7870c85
bautistagorchs <bautista.gorchs@gmail.com>
2024-03-02 12:13:51 -0300

    add functionality to register component

--- src/components/deliveryMan/register/Register.tsx
+++ src/components/deliveryMan/register/Register.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import React, { useState, ChangeEvent } from "react";
-import s from "./register.module.scss";
-import Header from "commons/header/Header";
-import UploadImage from "assets/img/UploadImage";
-import ButtonDarkBlue from "commons/buttonDarkBlue/ButtonDarkBlue";
-import Link from "next/link";
-import Eye from "assets/img/Eye";
 import ClosedEye from "assets/img/ClosedEye";
+import Eye from "assets/img/Eye";
+import UploadImage from "assets/img/UploadImage";
 import axios from "axios";
+import ButtonDarkBlue from "commons/buttonDarkBlue/ButtonDarkBlue";
+import Header from "commons/header/Header";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { ChangeEvent, useState } from "react";
+import { Toaster, toast } from "sonner";
+import s from "./register.module.scss";
 
 interface FormData {
   name: string;
@@ -22,12 +24,15 @@ interface FormData {
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 const Register: React.FC = () => {
+  const router = useRouter();
+
   const [error, setError] = useState<string | null>(null);
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const [showConfirmPassword, setShowConfirmPassword] =
     useState<boolean>(false);
   const [visitedInput1, setVisitedInput1] = useState(false);
   const [visitedInput2, setVisitedInput2] = useState(false);
+  const [loader, setLoader] = useState(false);
   const [formData, setFormData] = useState<FormData>({
     name: "",
     last_name: "",
@@ -83,12 +88,22 @@ const Register: React.FC = () => {
 
       return;
     } else {
+      setLoader(true);
       axios
         .post(`http://localhost:5000/api/users/register`, formData, {
           withCredentials: true,
         })
-        .then((response) => console.log(response))
-        .catch((err) => console.error(err));
+        .then(() => {
+          toast.success("Registro exitoso, revisa tu casilla de mail");
+          setLoader(false);
+          setTimeout(() => {
+            router.push("/login");
+          }, 2000);
+        })
+        .catch((err) => {
+          setLoader(false);
+          toast.error(err.response?.data);
+        });
     }
 
     if (formData.password !== formData.rep_password) {
@@ -102,139 +117,135 @@ const Register: React.FC = () => {
     }
   };
 
-  //   let temp = { ...formData };
-  //   userServiceRegister(temp)
-  //     .then(() => {
-  //       Swal.fire({
-  //         title: "Usuario creado con éxito!",
-  //         text: "Revisa tu correo para confirmar tu cuenta.",
-  //         icon: "success",
-  //       }).then(() => {
-  //         navigate.push("/login");
-  //       });
-  //     })
-  //     .catch((err) => {
-  //       if (err.response.data === "Email already exists") {
-  //         setError("Esta cuenta ya existe. Inicia sesión.");
-  //       } else if (err.response.data === "DNI already exists") {
-  //         setError(
-  //           "Ya se encuentra registrada una cuenta con ese Dni. Inicia sesión."
-  //         );
-  //       } else setError("Error al intentar registrarse.");
-  //     });
-  // };
-
   return (
     <div className={s.registerContainer}>
       <Header text="Creá tu cuenta" />
       <div className={s.contentContainer}>
-        <div className={s.content}>
-          <div className={s.iconContainer}>
-            <UploadImage />
-          </div>
-          <form
-            onSubmit={handleSubmit}
-            action="submint"
-            className={s.registerForm}
-          >
-            <input
-              type="text"
-              placeholder={
-                error && !formData.name ? "Ingrese su Nombre." : "Nombre"
-              }
-              className={
-                error && !formData.name ? `${s.errorText}` : `${s.normalText}`
-              }
-              value={formData.name}
-              onChange={(e) => handleInputChange(e, "name")}
-              autoFocus
-            />
-
-            <input
-              type="text"
-              placeholder={
-                error && !formData.last_name
-                  ? "Ingrese su Apellido."
-                  : "Apellido"
-              }
-              className={
-                error && !formData.last_name
-                  ? `${s.errorText}`
-                  : `${s.normalText}`
-              }
-              value={formData.last_name}
-              onChange={(e) => handleInputChange(e, "last_name")}
-            />
-
-            <input
-              type="email"
-              placeholder={
-                error && !formData.email
-                  ? "Ingrese una direccion de email valida"
-                  : !emailRegex.test(formData.email)
-                  ? "ejemplo@email.com"
-                  : "Ingrese un direccion de email valida."
-              }
-              className={
-                error && !formData.email ? `${s.errorText}` : `${s.normalText}`
-              }
-              value={formData.email}
-              onChange={(e) => handleInputChange(e, "email")}
-            />
-
-            <div className={s.inputPasswordContainer}>
-              <input
-                type={showPassword ? `text` : `password`}
-                className={s.inputPassword}
-                placeholder="**********"
-                value={formData.password}
-                onChange={(e) => handleInputChange(e, "password")}
-                onClick={() => setVisitedInput1(true)}
-              />
-              <div
-                className={s.eyeContainer}
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? <ClosedEye /> : <Eye />}{" "}
-              </div>
+        <div className={`${s.layer} ${loader && s.blur}`}>
+          <div className={s.content}>
+            <div className={s.iconContainer}>
+              <UploadImage />
             </div>
-            <div className={s.inputPasswordContainer}>
+            <form
+              onSubmit={handleSubmit}
+              action="submint"
+              className={s.registerForm}
+            >
               <input
-                type={showConfirmPassword ? `text` : `password`}
-                className={s.inputPassword}
-                placeholder="Confirmar contraseña"
-                value={formData.rep_password}
-                onChange={(e) => handleInputChange(e, "rep_password")}
-                onClick={() => setVisitedInput2(true)}
+                type="text"
+                placeholder={
+                  error && !formData.name ? "Ingrese su Nombre." : "Nombre"
+                }
+                className={
+                  error && !formData.name ? `${s.errorText}` : `${s.normalText}`
+                }
+                value={formData.name}
+                onChange={(e) => handleInputChange(e, "name")}
+                autoFocus
               />
-              <div
-                className={s.eyeContainer}
-                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-              >
-                {showConfirmPassword ? <ClosedEye /> : <Eye />}
+
+              <input
+                type="text"
+                placeholder={
+                  error && !formData.last_name
+                    ? "Ingrese su Apellido."
+                    : "Apellido"
+                }
+                className={
+                  error && !formData.last_name
+                    ? `${s.errorText}`
+                    : `${s.normalText}`
+                }
+                value={formData.last_name}
+                onChange={(e) => handleInputChange(e, "last_name")}
+              />
+
+              <input
+                type="email"
+                placeholder={
+                  error && !formData.email
+                    ? "Ingrese una direccion de email valida"
+                    : !emailRegex.test(formData.email)
+                    ? "ejemplo@email.com"
+                    : "Ingrese un direccion de email valida."
+                }
+                className={
+                  error && !formData.email
+                    ? `${s.errorText}`
+                    : `${s.normalText}`
+                }
+                value={formData.email}
+                onChange={(e) => handleInputChange(e, "email")}
+              />
+
+              <div className={s.inputPasswordContainer}>
+                <input
+                  type={showPassword ? `text` : `password`}
+                  className={s.inputPassword}
+                  placeholder="**********"
+                  value={formData.password}
+                  onChange={(e) => {
+                    setVisitedInput1(true);
+                    return handleInputChange(e, "password");
+                  }}
+                  onClick={() => setVisitedInput1(true)}
+                />
+                <div
+                  className={s.eyeContainer}
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <ClosedEye /> : <Eye />}{" "}
+                </div>
               </div>
-              {visitedInput2 && formData.password !== formData.rep_password && (
-                <div className={s.errorText}>Las contraseñas no coinciden.</div>
-              )}
-              {!/^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$/.test(
-                formData.password
-              ) &&
-                visitedInput1 && (
-                  <div className={s.errorText}>
-                    La contraseña debe contener 8 caracteres,
-                    <br /> una Mayuscula, una minuscula y un numero.
-                  </div>
-                )}
+              <div className={s.inputPasswordContainer}>
+                <input
+                  type={showConfirmPassword ? `text` : `password`}
+                  className={s.inputPassword}
+                  placeholder="Confirmar contraseña"
+                  value={formData.rep_password}
+                  onChange={(e) => {
+                    setVisitedInput2(true);
+                    return handleInputChange(e, "rep_password");
+                  }}
+                  onClick={() => setVisitedInput2(true)}
+                />
+                <div
+                  className={s.eyeContainer}
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                >
+                  {showConfirmPassword ? <ClosedEye /> : <Eye />}
+                </div>
+                {visitedInput2 &&
+                  formData.password !== formData.rep_password && (
+                    <div className={s.errorText}>
+                      Las contraseñas no coinciden.
+                    </div>
+                  )}
+                {!/^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$/.test(
+                  formData.password
+                ) &&
+                  visitedInput1 && (
+                    <div className={s.errorText}>
+                      La contraseña debe contener 8 caracteres,
+                      <br /> una Mayuscula, una minuscula y un numero.
+                    </div>
+                  )}
+              </div>
+            </form>
+            <div className={s.firstButtonContainer} onClick={handleSubmit}>
+              <ButtonDarkBlue text="Crear" />
             </div>
-          </form>
-          <div className={s.firstButtonContainer} onClick={handleSubmit}>
-            <ButtonDarkBlue text="Crear" />
+            <Link href={"/login"}>
+              <button className={s.loginButton}>Iniciar sesión</button>
+            </Link>
           </div>
-          <Link href={"/delivery-man/start-work-day"}>
-            <button className={s.loginButton}>Iniciar sesión</button>
-          </Link>
         </div>
+        <div
+          className={s.spinner}
+          style={{ display: loader ? "block" : "none" }}
+        ></div>
       </div>
+      <Toaster richColors position="top-center" expand={true} />
     </div>
   );
 };
