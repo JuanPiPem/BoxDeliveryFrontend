2ac72b6bec7c70683e66b43b24bc716b69350849
rosmari ledesma <rochilesde@gmail.com>
2024-03-05 18:57:44 -0300

    add affidavit view

---
+++ src/app/delivery-man/sworn-declaration/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SwornDeclaration from "components/deliveryMan/swornDeclaration/SwornDeclaration";
+
+const swornDeclaration = ()=>{
+
+  return(
+    <SwornDeclaration />
+  )
+}
+
+export default swornDeclaration
